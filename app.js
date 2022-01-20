4d86688532d1bd98d497c00c84d112e03a04365c
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-01-20 12:22:39 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+require('express-async-errors');
 const express = require('express');
 const app = express();
-
+require('./startup/logging')();
+require('./startup/app-use')(app);
+require('./startup/db')();
+require('./startup/prod')(app);
 
 
 
@@ -9,11 +13,10 @@ const port = process.env.PORT || 3000;
 app.listen(port);
 
 
-app.set('view engine', 'ejs');
+
 //app.use(expressLayouts);
 //app.set('layout', 'layouts/layout');
-app.use('/folder', express.static('folder'));
-app.use(express.urlencoded({ extended: false }));
 
 
-app.use('/', require('./routes/route'));
+
+
