9ca04d4015f1bf9ed188540c8c0cb6200cea45e0
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-01-24 18:37:17 +0000

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -6,8 +6,8 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const bcrypt = require('bcrypt');
 const joi = require('joi');
-const { genericFlash, conditionalFlash } = require('../middleware/flash');
-const { signFlash } = require('../middleware/flash');
+const { genericFlash, conditionalFlash } = require('../middleware/flash-messages');
+const { signFlash } = require('../middleware/flash-messages');
 const { ensureAuthenticated } = require('../middleware/auth');
 const passport = require('passport');
 require('../middleware/passport')(passport);
@@ -26,6 +26,8 @@ router.use(flash());
 router.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
+    res.locals.errors = req.flash('errors');
+    res.locals.errorsIntro = req.flash('errorsIntro');
     res.locals.messageIntro = req.flash('messageIntro');
     res.locals.message = req.flash('message');
     delete req.flash();
@@ -68,24 +70,7 @@ router.post('/login', async (req, res, next) => {
         req.flash('messageIntro', msg.intro);
         req.flash('message', msg.message);
         res.redirect('/login')
-    }
-    /*let valid;
-    let user = await User.User.findOne({ email: req.body.email })
-    if (!user) {
-        //req.session.message = genericFlash(2);
-        const msg = genericFlash(2);
-        req.flash('messageIntro', msg.intro);
-        req.flash('message', msg.message);
-        res.status(400).redirect('/login');
-    }
-
-    else { valid = await bcrypt.compare(req.body.password, user.password); }
-
-    if (!valid) {
-        //req.session.message = genericFlash(2);
-        res.redirect('/login')
-    }*/
-    else {
+    } else {
         passport.authenticate('local', {
             successRedirect: '/users/home',
             failureRedirect: '/login',
@@ -94,6 +79,56 @@ router.post('/login', async (req, res, next) => {
 
 
     }
+
+});
+
+// SIGN UP 
+router.post('/signup', async (req, res) => {
+    const schema = joi.object().keys({
+        username: joi.string().trim().min(3).max(50).required(),
+        email: joi.string().trim().email().required(),
+        password: joi.string().min(7).required(),
+        password_repeat: joi.string(),
+    });
+    const value = schema.validate(req.body);
+    debug(value)
+    if (value.error) {
+        const msg = conditionalFlash(value.error.message);
+        req.flash('errorsIntro', msg.intro);
+        req.flash('errors', msg.message);
+        res.status(400).redirect('/signup');
+    }
+    else if (req.body.password != req.body.password_repeat) {
+        const msg = genericFlash(4);
+        req.flash('messageIntro', msg.intro);
+        req.flash('message', msg.message);
+        res.status(400).redirect('/signup');
+    }
+    else {
+        let user = new User.User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt)
+        try {
+            user = await user.save()
+            const msg = genericFlash(3);
+            req.flash('success', msg.intro);
+            debug(user)
+            res.redirect(302, '/login');
+            res.end();
+        } catch (error) {
+            const msg = signFlash(error.message);
+            req.flash('errorsIntro', msg.intro);
+            req.flash('errors', msg.message);
+            res.redirect(302, '/signup')
+        }
+
+    }
+
+
 
 });
 module.exports = router;
