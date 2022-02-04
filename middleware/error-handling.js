f964eb85f58c58db03d64fb81d4cabb7da89622a
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-02-04 12:14:35 +0000

    refactored

--- middleware/error-handling.js
+++ middleware/error-handling.js
@@ -5,16 +5,4 @@ module.exports = function (error, req, res, next) {
     winston.error(error.message, error)
     debug('An Unexpected Error Occured')
     res.status(500).end();//send('An Unexpected Error Occured');
-
 };
-
-/*function asyncError (handler) {
-    return async (req, res, next) => {
-    try {
-        await handler;
-    } catch (error) {
-        console.log('i am working ')
-        next(error)
-    }
-}
-}*/
