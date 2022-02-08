c22da4b2d084cc0f0370db8646f968bb809b5188
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-02-08 11:06:37 +0000

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -58,9 +58,9 @@ router.get('/home/products', (req, res) => {
 router.get('/home/dashboard', ensureAuthenticated, (req, res) => {
     res.render('dashboard');
 });
-router.get('/logout', (rq, rs) => {
-    rq.logout();
-    rs.redirect('/');
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 });
 
 // POST ROUTES
