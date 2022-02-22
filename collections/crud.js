89bcfe2bb5c30ca41958727fc061d1a18ff66742
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-02-22 17:02:39 +0000

    renamed file

---
+++ collections/crud.js
@@ -0,0 +1,20 @@
+const User = require('../schemas/user');
+
+
+
+const userboardPost = async (req, res) => {
+    const user = await User.User.findByIdAndUpdate(req.user.id, {
+        $set: {
+            username: req.body.username,
+            Fname: req.body.Fname,
+            Lname: req.body.Lname,
+            bio: req.body.bio,
+        },
+    }, { new: true })
+    console.log(user);
+    res.redirect('/home/user')
+}
+
+module.exports = {
+    userboardPost,
+}
