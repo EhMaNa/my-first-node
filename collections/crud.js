1d51c7a6e311559086a13869e96e28971fb07ac5
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-02-23 12:20:13 +0000

    Update crud.js

--- collections/crud.js
+++ collections/crud.js
@@ -42,6 +42,46 @@ const loginPost = async (req, res, next) => {
 
 }
 
+const signupPost = async (req, res) => {
+    const schema = joi.object().keys({
+        username: joi.string().trim().min(3).max(20).required(),
+        email: joi.string().trim().email().required(),
+        password: joi.string().min(7).required(),
+        password_repeat: joi.string(),
+    });
+    const value = schema.validate(req.body);
+    debug(value)
+    if (value.error) {
+        reqFlash(req, conditionalFlash(value.error.message), 'errorsIntro', 'errors');
+        res.status(400).redirect('/signup');
+    }
+    else if (req.body.password != req.body.password_repeat) {
+        reqFlash(req, genericFlash(4), 'messageIntro', 'message');
+        res.status(400).redirect('/signup');
+    }
+    else {
+        let user = new User.User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt)
+        try {
+            user = await user.save()
+            const msg = genericFlash(3);
+            req.flash('success', msg.intro);
+            debug(user)
+            res.redirect(302, '/login');
+            res.end();
+        } catch (error) {
+            reqFlash(req, signFlash(error.message), 'errorsIntro', 'errors');
+            res.redirect(302, '/signup')
+        }
+
+    }
+}
+
 module.exports = {
     userboardPost,
     loginPost,
