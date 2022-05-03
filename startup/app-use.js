05fbcc0ea43bc024ca7e6cef8d11af389bcd244f
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2022-05-03 11:21:19 +0000

    final changes

--- startup/app-use.js
+++ startup/app-use.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const error = require('../middleware/error-handling')
 
-
 module.exports = function (app) {
     app.set('view engine', 'ejs');
     app.use('/folder', express.static('folder'));
